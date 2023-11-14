64023c40094450802f8fc557b5f2d148619657af
SyedMuhammedAbbas <66178330+SyedMuhammedAbbas@users.noreply.github.com>
2023-11-14 10:12:11 +0500

    additional services card template added

---
+++ components/Cards/AdditionalService.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const AdditionalService = () => {
+  return (
+    <article>
+      <div>
+        <Image />
+      </div>
+      <div></div>
+    </article>
+  );
+};
