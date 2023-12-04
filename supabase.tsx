1633685513aa1a1d545db0126337a8e94eef4c91
SyedMuhammedAbbas <66178330+SyedMuhammedAbbas@users.noreply.github.com>
2023-12-05 01:24:16 +0500

    supabase.tsx file commented

--- supabase.tsx
+++ supabase.tsx
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/@types/database.types";
+// import { createClient } from "@supabase/supabase-js";
+// import { Database } from "@/@types/database.types";
 
-// Create a single supabase client for interacting with your database
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+// // Create a single supabase client for interacting with your database
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+// const supabase = createClient<Database>(supabaseUrl, supabaseKey);
