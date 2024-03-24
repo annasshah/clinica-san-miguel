0bfdb4563b36c55e70f6f3a0276a8aa38f420e55
Ali-Aizaz <50923380+Ali-Aizaz@users.noreply.github.com>
2024-03-25 04:24:58 +0500

    fixed i18 issue

--- app/[locale]/career/page.tsx
+++ app/[locale]/career/page.tsx
@@ -2,9 +2,7 @@ import { career_cover } from "@/assets/images";
 import { Locations, Services, Testimonials } from "@/sections";
 
 import Image from "next/image";
-// import { useEffect, useRef, useState } from "react";
 import { styles } from "../styles";
-import TranslationsProvider from "@/components/TranslationsProvider";
 import { Opportunities } from "@/sections/Career/Opportunities";
 import initTranslations from "@/app/i18n";
 
@@ -33,63 +31,55 @@ const Career = async ({
   //   "Medical Assistant",
   //   "Primary Care Physician ( MD ) Doctor of Medicine",
   // ];
-  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+  const { t } = await initTranslations(locale, i18nNamespaces);
 
   return (
-    <TranslationsProvider
-      namespaces={i18nNamespaces}
-      locale={locale}
-      resources={resources}
-    >
-      <main>
-        <section className="flex flex-col relative w-full gap-10">
-          <div className="flex flex-col gap-1 justify-center items-center">
-            <h1 className={`${styles.sectionHeadText} text-[#C1001F]`}>
-              {t("Career")}
+    <main>
+      <section className="flex flex-col relative w-full gap-10">
+        <div className="flex flex-col gap-1 justify-center items-center">
+          <h1 className={`${styles.sectionHeadText} text-[#C1001F]`}>
+            {t("career_title")}
+          </h1>
+        </div>
+        <article
+          style={{ aspectRatio: "2.5 / 1" }}
+          className="left-0 w-[96%] md:w-[90%] lg:w-[85%] xl:w-[80%] rounded-r-full bg-[#ccc]"
+        >
+          <Image
+            src={career_cover}
+            alt=""
+            style={{ aspectRatio: "2.5 / 1" }}
+            className="bg-cover object-cover w-full object-center rounded-r-full"
+          />
+        </article>
+        <article className="flex flex-col gap-4 w-full items-center px-7">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className={`${styles.sectionHeadText} text-customGray`}>
+              {t("career_sub_title")}
             </h1>
+            <p className={`${styles.sectionSubText} text-[#000000]`}>
+              {t(
+                "Thanks for your interest in job opportunities at Clinica San Miguel"
+              )}
+            </p>
           </div>
 
-          <article
-            style={{ aspectRatio: "2.5 / 1" }}
-            className="left-0 w-[96%] md:w-[90%] lg:w-[85%] xl:w-[80%] rounded-r-full bg-[#ccc]"
-          >
-            <Image
-              src={career_cover}
-              alt=""
-              style={{ aspectRatio: "2.5 / 1" }}
-              className="bg-cover object-cover w-full object-center rounded-r-full"
-            />
-          </article>
-
-          <article className="flex flex-col gap-4 w-full items-center px-7">
-            <div className="flex flex-col items-center justify-center gap-4">
-              <h1 className={`${styles.sectionHeadText} text-customGray`}>
-                {t("Join Our Team!")}
-              </h1>
-              <p className={`${styles.sectionSubText} text-[#000000]`}>
-                {t(
-                  "Thanks for your interest in job opportunities at Clinica San Miguel"
-                )}
-              </p>
-            </div>
-
-            <div className="flex flex-col items-start gap-2">
-              <p className="text-[25px] text-[#000000] font-poppins">
-                {t("Current Opportunities:")}
-              </p>
-              <ul className="flex flex-col items-start">
-                <Opportunities />
-              </ul>
-            </div>
-          </article>
-        </section>
-        <div className="w-full flex justify-center">
-          <Services />
-        </div>
-        <Locations />
-        <Testimonials headingFlag={true} mode={"light"} />
-      </main>
-    </TranslationsProvider>
+          <div className="flex flex-col items-start gap-2">
+            <p className="text-[25px] text-[#000000] font-poppins">
+              {t("Current Opportunities:")}
+            </p>
+            <ul className="flex flex-col items-start">
+              <Opportunities />
+            </ul>
+          </div>
+        </article>
+      </section>
+      <div className="w-full flex justify-center">
+        <Services />
+      </div>
+      <Locations />
+      <Testimonials headingFlag={true} mode={"light"} />
+    </main>
   );
 };
 
